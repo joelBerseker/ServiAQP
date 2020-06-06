b598151c4cf53df17bf6ed3208fda325731be426
joelBerseker <jatamaria@unsa.edu.pe>
2020-06-06 17:44:29 -0500

    Autoincrementable

--- tablas.sql
+++ tablas.sql
@@ -5,4 +5,6 @@ CREATE TABLE `categoria` (
   `CatEstReg` char(1) NOT NULL DEFAULT 'A',
   `CatFecCre` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+ALTER TABLE `categoria`
+  MODIFY `CatId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
