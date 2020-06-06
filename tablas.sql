e4a0b344a6075762a90a1ffcba776a9503d6317b
joelBerseker <jatamaria@unsa.edu.pe>
2020-06-06 12:07:05 -0500

    Categorias en tablas.sql mejorado

--- tablas.sql
+++ tablas.sql
@@ -1,6 +1,8 @@
-CREATE TABLE `serviaqp`.`categoria` (
-    `CatId` INT NOT NULL AUTO_INCREMENT ,
-    `CatNom` VARCHAR(60) NOT NULL ,
-    `CatDes` TEXT NOT NULL ,
-    PRIMARY KEY (`CatId`)
-) ENGINE = InnoDB;
+CREATE TABLE `categoria` (
+  `CatId` int(11) NOT NULL,
+  `CatNom` varchar(60) NOT NULL,
+  `CatDes` text NOT NULL,
+  `CatEstReg` char(1) NOT NULL DEFAULT 'A',
+  `CatFecCre` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
