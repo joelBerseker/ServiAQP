dd3a9dfd3a065f3ff6c6eceeffc4e92d84aef7d3
leowi <jhoshep_1999@hotmail.com>
2020-06-06 17:16:30 -0500

    cambios al diseño

--- index.php
+++ index.php
@@ -50,6 +50,7 @@ include("includes/header.php");
 
 <div class="section-index ">
 	<div class="container">
+		<hr class="" width="100%">
 		<div>
 			<h5>Destacados</h5>
 			<p>Prueba los servicios destacados que tenemos para ti</p>
@@ -61,7 +62,10 @@ include("includes/header.php");
 							<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');">
 							</div>
 							<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
-					-->	
+					-->	<div class="card-img-overlay">
+							<a class="btn  btn-sm informacion-btn">$ 23.30</a>
+							<a class="btn  btn-sm informacion-btn">4.0</a>
+  						</div>
 						</div>
 						<div class="card-body text-center">
 							<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
@@ -73,125 +77,153 @@ include("includes/header.php");
 					</div>
 				</div>
 				<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
-				<div class="card mb-4 border-0 ">
-					<div class="img-animtion">
-						<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');"></div>
-						<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
-				-->
-					</div>
-					<div class="card-body text-center">
-						<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
-						<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
-				
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
-				<div class="card mb-4 border-0 ">
-					<div class="img-animtion">
-						<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');"></div>
-						<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
-				-->
-					</div>
-					<div class="card-body text-center">
-						<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
-						<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
-				
+					<div class="card mb-4 border-0 ">
+						<div class="img-animtion">
+							<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');">
+							</div>
+							<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
+					-->	<div class="card-img-overlay">
+							<a class="btn  btn-sm informacion-btn">$ 23.30</a>
+							<a class="btn  btn-sm informacion-btn">4.0</a>
+  						</div>
+						</div>
+						<div class="card-body text-center">
+							<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
+							<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
+					
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
-				<div class="card mb-4 border-0 ">
-					<div class="img-animtion">
-						<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');"></div>
-						<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
-				-->
-					</div>
-					<div class="card-body text-center">
-						<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
-						<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
-				
+				<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+					<div class="card mb-4 border-0 ">
+						<div class="img-animtion">
+							<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');">
+							</div>
+							<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
+					-->	<div class="card-img-overlay">
+							<a class="btn  btn-sm informacion-btn">$ 23.30</a>
+							<a class="btn  btn-sm informacion-btn">4.0</a>
+  						</div>
+						</div>
+						<div class="card-body text-center">
+							<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
+							<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
+					
+						</div>
 					</div>
 				</div>
-			</div>
+				<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+					<div class="card mb-4 border-0 ">
+						<div class="img-animtion">
+							<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');">
+							</div>
+							<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
+					-->	<div class="card-img-overlay">
+							<a class="btn  btn-sm informacion-btn">$ 23.30</a>
+							<a class="btn  btn-sm informacion-btn">4.0</a>
+  						</div>
+						</div>
+						<div class="card-body text-center">
+							<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
+							<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
+					
+						</div>
+					</div>
+				</div>
 		</div>
-
+		<hr class="" width="100%">
 		<div>
 			<h5>Elecciones del editor</h5>
 			<p>Prueba los servicios que los editores tienen para ti</p>
 		</div>
 		<div class="row justify-content-center">
 
-			<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
-				<div class="card mb-4 border-0 ">
-					<div class="img-animtion">
-						<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');"></div>
-						<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
-				-->
-					</div>
-					<div class="card-body text-center">
-						<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
-						<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
-				
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
-				<div class="card mb-4 border-0 ">
-					<div class="img-animtion">
-						<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');"></div>
-						<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
-				-->
-					</div>
-					<div class="card-body text-center">
-						<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
-						<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
-				
+		<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+					<div class="card mb-4 border-0 ">
+						<div class="img-animtion">
+							<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');">
+							</div>
+							<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
+					-->	<div class="card-img-overlay">
+							<a class="btn  btn-sm informacion-btn">$ 23.30</a>
+							<a class="btn  btn-sm informacion-btn">4.0</a>
+  						</div>
+						</div>
+						<div class="card-body text-center">
+							<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
+							<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
+					
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
-				<div class="card mb-4 border-0 ">
-					<div class="img-animtion">
-						<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');"></div>
-						<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
-				-->
-					</div>
-					<div class="card-body text-center">
-						<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
-						<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
-				
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
-				<div class="card mb-4 border-0 ">
-					<div class="img-animtion">
-						<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');"></div>
-						<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
-				-->
-					</div>
-					<div class="card-body text-center">
-						<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
-						<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
-						<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
-				
+				<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+					<div class="card mb-4 border-0 ">
+						<div class="img-animtion">
+							<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');">
+							</div>
+							<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
+					-->	<div class="card-img-overlay">
+							<a class="btn  btn-sm informacion-btn">$ 23.30</a>
+							<a class="btn  btn-sm informacion-btn">4.0</a>
+  						</div>
+						</div>
+						<div class="card-body text-center">
+							<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
+							<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
+					
+						</div>
 					</div>
 				</div>
-			</div>
+				<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+					<div class="card mb-4 border-0 ">
+						<div class="img-animtion">
+							<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');">
+							</div>
+							<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
+					-->	<div class="card-img-overlay">
+							<a class="btn  btn-sm informacion-btn">$ 23.30</a>
+							<a class="btn  btn-sm informacion-btn">4.0</a>
+  						</div>
+						</div>
+						<div class="card-body text-center">
+							<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
+							<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
+					
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+					<div class="card mb-4 border-0 ">
+						<div class="img-animtion">
+							<div class="imageny" style="background-image:url('https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/137877433/original/3f9ec5c88603b6da99a31f15da29351f19a73ac9/write-travel-and-leisure-content.png');">
+							</div>
+							<!--<img src="https://images-na.ssl-images-amazon.com/images/I/51bWIMdXiML.jpg	" alt="Card image cap" class="card-img-top">
+					-->	<div class="card-img-overlay">
+							<a class="btn  btn-sm informacion-btn">$ 23.30</a>
+							<a class="btn  btn-sm informacion-btn">4.0</a>
+  						</div>
+						</div>
+						<div class="card-body text-center">
+							<h3 class="card-title">	Clases de escritura d sfsd dfsdfsd fds sf </h3>
+							<textarea disabled class="descrip text-center">Enc das das adsa sdasd adsa sdasd sadasdasd asdasdasdasdasd a das dusdfsfsasdasd a dsa da as dfsfsfentra los mejores juegos de plataforma que puedas encontrar</textarea>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm">Ver más</a><a href=""></a>
+							<a href="/Categoria/VerProductos?id=8" class="btn btn-primary btn-sm"><span class=" icon-heart"></span></a><a href=""></a>
+					
+						</div>
+					</div>
+				</div>
 		</div>
 		
 			
