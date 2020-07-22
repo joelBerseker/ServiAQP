ff4c256194f853b315bddd84f44d40d67043dcf8
Sergio Rondon <sergio19980816@gmail.com>
2020-07-22 16:32:54 -0500

    CAMBIOS EN BOOTSPTRA

--- chat/index.php
+++ chat/index.php
@@ -1,17 +1,69 @@
 <?php
 include("../includes/sesion.php");
+include("../includes/data_base.php");
 include("../includes/global_variable.php");
+?>
+<?php
 
+if(isset($_GET['leido'])) {
+  $leido = mysql_real_escape_string($_GET['leido']);
+  $usuariod = mysql_real_escape_string($_GET['usuario']);
+
+  $tchats = mysql_query("SELECT * FROM chats WHERE de = '$usuariod' OR para = '$usuariod'");
+  $tc = mysql_fetch_array($tchats);
+  if($tc['de'] != $_SESSION['id']) {
+  $update = mysql_query("UPDATE chats SET leido = '1' WHERE de = '$usuariod' OR para = '$usuariod'");
+  }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Chats</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="../frontend/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel
+  
+  ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- fullCalendar 2.2.5-->
+  <link rel="stylesheet" href="../frontend/plugins/fullcalendar/fullcalendar.min.css">
+  <link rel="stylesheet" href="../frontend/plugins/fullcalendar/fullcalendar.print.css" media="print">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../frontend/dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="../frontend/dist/css/skins/_all-skins.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="../frontend/plugins/iCheck/flat/blue.css">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<?php
 include('../includes/navbar.php');
-$inicio = true;
-$titulo_html = "Inicio";
-include("../includes/header.php");
+$titulo_html = "Servicios";
+include('../includes/header.php');
+include("../includes/data_base.php");
 ?>
 
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+  <div class="section">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
+    <section class="container">
       <h1>
         Chat
         <small>13 nuevos mensajes</small>
@@ -67,17 +119,19 @@ include("../includes/header.php");
             <div class="box-body">
               <!-- Conversations are loaded here -->
               <div class="direct-chat-messages" style="overflow: scroll; height: 400px;">
-
-                
+              
                 <?php
-                $user = mysql_real_escape_string($_GET['usuario']);
-                $sess = $_SESSION['id'];
-                $chats = mysql_query("SELECT * FROM chats WHERE de = '$user' AND para = '$sess' OR de = '$sess' AND para = '$user' order by id_cha desc");
-                while($ch = mysql_fetch_array($chats)) { 
-
-                  if($ch['de'] == $user) {$var = $user;} else {$var = $sess;}
-                  $usere = mysql_query("SELECT * FROM usuarios WHERE id_use = '$var'");
-                  $us = mysql_fetch_array($usere);
+                if(isset($usuario)){
+                  $user = mysqli_real_escape_string($conn, $_GET['usuario']);
+                  $sess = $_SESSION['id'];
+                  $chats = mysql_query("SELECT * FROM chats WHERE de = '$user' AND para = '$sess' OR de = '$sess' AND para = '$user' order by id_cha desc");
+                  while($ch = mysql_fetch_array($chats)) { 
+  
+                    if($ch['de'] == $user) {$var = $user;} else {$var = $sess;}
+                    $usere = mysql_query("SELECT * FROM usuarios WHERE id_use = '$var'");
+                    $us = mysql_fetch_array($usere);
+                }
+                
                 ?>
   
 
@@ -141,23 +195,31 @@ include("../includes/header.php");
 
               <?php
               if(isset($_POST['enviar'])) {
-
-                $mensaje = mysql_real_escape_string($_POST['mensaje']);
+                if(isset($_POST['mensaje'])) {
+                $mensaje = mysqli_real_escape_string($conn, $_POST['mensaje']);
+                }
+                if(isset($_POST['id'])) {
                 $de = $_SESSION['id'];
-                $para = mysql_real_escape_string($_GET['usuario']);
-
-                $comprobar = mysql_query("SELECT * FROM c_chats WHERE de = '$de' AND para = '$para' OR de = '$para' AND para = '$de'");
-                $com = mysql_fetch_array($comprobar);
-                if(mysql_num_rows($comprobar) == 0) {
-                  $insert = mysql_query("INSERT INTO c_chats (de,para) VALUES ('$de','$para')");
-                  $siguiente = mysql_query("SELECT id_cch FROM c_chats WHERE de = '$de' AND para = '$para' OR de = '$para' AND para = '$de'");
-                  $si = mysql_fetch_array($siguiente);
-                  $insert2 = mysql_query("INSERT INTO chats (id_cch,de,para,mensaje,fecha,leido) VALUES ('".$si['id_cch']."','$de','$para','$mensaje',now(),'0')");
+                }
+                if(isset($_POST['usuario'])) {
+                $para = mysqli_real_escape_string($conn, $_GET['usuario']);
+                }
+                if(isset($_POST['comprobar'])) {
+                $comprobar = mysqli_query($conn, "SELECT * FROM c_chats WHERE de = '$de' AND para = '$para' OR de = '$para' AND para = '$de'");
+                
+                $com = mysqli_fetch_array($comprobar);
+                
+                if(mysqli_num_rows($comprobar) == 0) {
+                  $insert = mysqli_query($conn, "INSERT INTO c_chats (de,para) VALUES ('$de','$para')");
+                  $siguiente = mysqli_query($conn, "SELECT id_cch FROM c_chats WHERE de = '$de' AND para = '$para' OR de = '$para' AND para = '$de'");
+                  $si = mysqli_fetch_array($siguiente);
+                  $insert2 = mysqli_query($conn,"INSERT INTO chats (id_cch,de,para,mensaje,fecha,leido) VALUES ('".$si['id_cch']."','$de','$para','$mensaje',now(),'0')");
                   if($insert) {echo '<script>window.location="chat.php?usuario='.$para.'"</script>';}
                 }
+              }
                 else
                 {
-                  $insert3 = mysql_query("INSERT INTO chats (id_cch,de,para,mensaje,fecha,leido) VALUES ('".$com['id_cch']."','$de','$para','$mensaje',now(),'0')");
+                  $insert3 = mysqli_query($conn, "INSERT INTO chats (id_cch,de,para,mensaje,fecha,leido) VALUES ('".$com['id_cch']."','$de','$para','$mensaje',now(),'0')");
                   if($insert3) {echo '<script>window.location="chat.php?usuario='.$para.'"</script>';}
                 }
 
@@ -166,6 +228,230 @@ include("../includes/header.php");
               }
 
               ?>
+
+
+            </div>
+            <!-- /.box-footer-->
+            
+        </div>
+        <!-- /.col -->
+
+
+
+
+
+
+
+              </div>
+              <!-- /.mailbox-read-message -->
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /. box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Create the tabs -->
+    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+      <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
+      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <!-- Home tab content -->
+      <div class="tab-pane" id="control-sidebar-home-tab">
+        <h3 class="control-sidebar-heading">Recent Activity</h3>
+        <ul class="control-sidebar-menu">
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-birthday-cake bg-red"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
+
+                <p>Will be 23 on April 24th</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-user bg-yellow"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
+
+                <p>New phone +1(800)555-1234</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
+
+                <p>nora@example.com</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-file-code-o bg-green"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
+
+                <p>Execution time 5 seconds</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+        <h3 class="control-sidebar-heading">Tasks Progress</h3>
+        <ul class="control-sidebar-menu">
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Custom Template Design
+                <span class="label label-danger pull-right">70%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Update Resume
+                <span class="label label-success pull-right">95%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-success" style="width: 95%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Laravel Integration
+                <span class="label label-warning pull-right">50%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Back End Framework
+                <span class="label label-primary pull-right">68%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+      </div>
+      <!-- /.tab-pane -->
+      <!-- Stats tab content -->
+      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
+      <!-- /.tab-pane -->
+      <!-- Settings tab content -->
+      <div class="tab-pane" id="control-sidebar-settings-tab">
+        <form method="post">
+          <h3 class="control-sidebar-heading">General Settings</h3>
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Report panel usage
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Some information about this general settings option
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Allow mail redirect
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Other sets of options are available
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Expose author name in posts
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Allow the user to show his name in blog posts
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <h3 class="control-sidebar-heading">Chat Settings</h3>
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Show me as online
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Turn off notifications
+              <input type="checkbox" class="pull-right">
+            </label>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Delete chat history
+              <a href="javascript:void(0)" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
+            </label>
+          </div>
+          <!-- /.form-group -->
+        </form>
+      </div>
+      <!-- /.tab-pane -->
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+
 <?php
-include("../includes/footer.php");
+include("../includes/footer.php")
 ?>
+
